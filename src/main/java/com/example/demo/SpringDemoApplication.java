9f27111c924c003e5ae990bf2aee3ee95bb029c3
sathvik reddy <sathvik2121@gmail.com>
2022-09-12 16:52:53 +0530

    adding

--- src/main/java/com/example/demo/SpringDemoApplication.java
+++ src/main/java/com/example/demo/SpringDemoApplication.java
@@ -3,25 +3,90 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.fop.apps.FOPException;
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.MimeConstants;
+import java.io.FileOutputStream;
+import com.microsoft.azure.storage.*;
+import com.microsoft.azure.storage.blob.*;
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.util.ImageIOUtil;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfReader;
+
+import com.itextpdf.signatures.BouncyCastleDigest;
+import com.itextpdf.signatures.DigestAlgorithms;
+import com.itextpdf.signatures.IExternalDigest;
+import com.itextpdf.signatures.IExternalSignature;
+import com.itextpdf.signatures.PdfSignatureAppearance;
+import com.itextpdf.signatures.PdfSigner;
+import com.itextpdf.signatures.PrivateKeySignature;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+
+import java.security.PrivateKey;
+import java.security.Security;
+
+import java.security.cert.Certificate;
+
 
 @RestController
 @SpringBootApplication
 public class SpringDemoApplication {
 
-	public static void main(String[] args) {
+	
+	public static void main(String[] args)  {
+		//SpringDemoApplication demo=new SpringDemoApplication();
+		//String message=demo.run();
+		//System.out.println(message);
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
-
 	@GetMapping("/")
-	public String welcome()
+	public String test()
 	{
-		return "welcome to the application";
+		return "hello";
+	}
+
+	@GetMapping("/testing")
+	public String run(@RequestParam String id)
+	{
+		return "ID: " + id;
+}
+
 	}
 	
-	@GetMapping("/test")
-	public String Congrats()
-	{
-		return "congratulations";
-	}
-}
+	
+
