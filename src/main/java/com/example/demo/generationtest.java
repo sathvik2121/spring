4d028834910700d2d279b4aee397586d096f6212
sathvik reddy <sathvik2121@gmail.com>
2022-12-09 18:36:15 +0530

    adding

--- src/main/java/com/example/demo/generationtest.java
+++ src/main/java/com/example/demo/generationtest.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -99,9 +100,10 @@ public class generationtest {
 		this.s=s;
 	}
 	}
+	@RequestMapping("/object")
 	class getdetails
 	{
-	@GetMapping("/object")
+	
 	public String getObj(@RequestParam(name="object")details Details)
 	{
 		return Details.a+"ok"+Details.s;
